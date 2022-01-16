ae123295419ccc8a02747b9a7fd57d73b4ab868f
Bradley <liu770807152@hotmail.com>
2022-01-17 01:03:15 +1100

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import './index.css';
 import Main from './pages/Main';
 
 ReactDOM.render(
 	<React.StrictMode>
+		<Helmet>
+			<meta charSet='utf-8' />
+			<meta name='description' content='Chris Blog Management System' />
+			<title>Chris Blog Management System</title>
+			<link rel='shortcut icon' href='%PUBLIC_URL%/favicon.ico' />
+		</Helmet>
 		<Main />
 	</React.StrictMode>,
 	document.getElementById('root')
