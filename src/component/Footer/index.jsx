56255b279e16f65e5eef63e207fb5db0634f663a
Bradley <liu770807152@hotmail.com>
2022-01-23 23:54:44 +1100

    finish footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,135 +1,36 @@
 import React, { useState } from "react";
+import footerData from '../../utils/footerData'
 import "./styles.scss";
-
-const toolsData = [
-  {
-    id: 3,
-    name: "VsCode",
-    describe: "我们现在主要开发工具，懂的都懂！",
-    iocn: "http://cdn.lululuting.com/upic/vscode-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/vscode.png",
-    bgColor: "#2C2C32",
-  },
-  {
-    id: 9,
-    name: "nextJs",
-    describe: "目前网站主要前端框架  React和NextJS！",
-    iocn: "http://cdn.lululuting.com/upic/next-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/next.png",
-    bgColor: "#fff",
-  },
-  {
-    id: 1,
-    name: "PS",
-    describe: "对，我就是在用ps，你打我呀！",
-    iocn: "http://cdn.lululuting.com/upic/ps-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/ps.png",
-    bgColor: "#38c8fe",
-  },
-  {
-    id: 4,
-    name: "Ant Design",
-    describe: "我们网站大量使用的UI库，Antd yyds！🙏",
-    iocn: "http://cdn.lululuting.com/upic/antd-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/antd.png",
-    bgColor: "#1890ff",
-  },
-  {
-    id: 5,
-    name: "哔哩哔哩",
-    describe: "我们文章中的所有视频都放在了B站！",
-    iocn: "http://cdn.lululuting.com/upic/bilibili-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/bilibili.png",
-    bgColor: "#fb7299",
-  },
-  {
-    id: 6,
-    name: "网易云音乐",
-    describe: "没错，左下角播放器的数据源是偷网易云音乐的！",
-    iocn: "http://cdn.lululuting.com/upic/wyyyy-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/wyyyy.png",
-    bgColor: "#fff",
-  },
-  {
-    id: 2,
-    name: "Github",
-    describe: "你能看到的，我们都开源在Github上了！",
-    iocn: "http://cdn.lululuting.com/upic/github.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/github-1.png",
-    bgColor: "#000",
-  },
-  {
-    id: 7,
-    name: "码云",
-    describe: "没有梯子，我们项目管理用的是码云，不是GitHub。",
-    iocn: "http://cdn.lululuting.com/upic/mayun-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/mayun.png",
-    bgColor: "#40485b",
-  },
-  {
-    id: 8,
-    name: "稿定设计",
-    describe: "我们网站几乎所有作图都是出自稿定设计，稿定打钱！",
-    iocn: "http://cdn.lululuting.com/upic/gaoding-1.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/gaoding.png",
-    bgColor: "#3260f4",
-  },
-  {
-    id: 10,
-    name: "和风天气",
-    describe: "白嫖的天气数据，真香！感谢和风天气！🙏",
-    iocn: "http://cdn.lululuting.com/upic/hftq.png",
-    hoverIcon: "http://cdn.lululuting.com/upic/hftq.png",
-    bgColor: "#fff",
-  },
-];
 
 const Footer = () => {
   const [active, setActive] = useState(null);
 
-  const onMouseEnter = (id) => {
-    setActive(id);
-  };
-  const onMouseLeave = () => {
-    setActive(null);
-  };
-
   return (
     <footer className="footer">
-      <div className="wrap" style={{ display: "flex", justifyContent: 'space-around'}}>
+      <div className="flex justify-around">
         <div className="footer-box">
           <div className="left-logo">
             <a href='/'>
-              <img className="logo" src="/static/logo1.png" alt='logo'></img>
+              <img className="logo" src="/logo_transparent.png" alt='logo'></img>
             </a>
           </div>
           <div className="right-memu">
             <div>
-              Copyright © 2021
-                <a>TGBK</a>
+              <span>Author: Chris</span>
             </div>
             <div>
-              <a
-                className="beian-a"
-                target="_blank"
-                href="/"
-              >
-                粤ICP备20008654号-1
-              </a>
+              <span>Copyright © 2022</span>
             </div>
           </div>
         </div>
-
         <div className="tools-box">
           <div className="inner">
             <div className="tool-list">
-              {toolsData.map((item) => (
-                <a
-									href='/'
+              {footerData.map((item) => (
+                <div
                   className="tool-item tool-notion1"
-                  data-color="red"
-                  onMouseEnter={() => onMouseEnter(item.id)}
-                  onMouseLeave={onMouseLeave}
+                  onMouseEnter={() => setActive(item.id)}
+                  onMouseLeave={() => setActive(null)}
                   key={item.id}
                   style={{
                     backgroundColor: `${
@@ -140,10 +41,7 @@ const Footer = () => {
                   <i
                     className="tool-icon"
                     style={{
-                      backgroundImage: `url(${
-                        active === item.id ? item.hoverIcon : item.iocn
-                      })`,
-                    }}
+                      backgroundImage: `url(${item.icon})`}}
                   />
                   <div className="tool-desc">
                     <div className="hide-desc" />
@@ -152,10 +50,10 @@ const Footer = () => {
                       style={item.bgColor === "#fff" ? { color: "#333" } : null}
                     >
                       <p className="name">{item.name}</p>
-                      <p className="describe">{item.describe}</p>
+                      <p className="describe">{item.description}</p>
                     </div>
                   </div>
-                </a>
+                </div>
               ))}
             </div>
           </div>
---
+++ src/utils/footerData.js
@@ -0,0 +1,60 @@
+const footerData = [
+	{
+		id: 0,
+		name: 'React',
+		description: 'Built with React Hooks & Redux',
+		icon: '/icons/react.webp',
+		bgColor: '#3b38e4',
+	},
+	{
+		id: 1,
+		name: 'Next.js',
+		description: 'React framework featuring server-side rendering',
+		icon: '/icons/next.webp',
+		bgColor: '#ffb8b8',
+	},
+	{
+		id: 2,
+		name: 'Egg.js',
+		description: 'Better enterprise frameworks with Node.js & Koa',
+		icon: '/icons/egg.webp',
+		bgColor: '#ff9900',
+	},
+	{
+		id: 3,
+		name: 'Ant Design',
+		description: 'A powerful and versatile UI library',
+		icon: '/icons/antd.webp',
+		bgColor: '#fff',
+	},
+	{
+		id: 4,
+		name: 'Sass',
+		description: 'Most mature, stable, and powerful CSS extension',
+		icon: '/icons/sass.webp',
+		bgColor: '#ffb8b8',
+	},
+	{
+		id: 5,
+		name: 'MySQL',
+		description: 'One of the most popular open source databases',
+		icon: '/icons/mysql.webp',
+		bgColor: '#3b38e4',
+	},
+	{
+		id: 6,
+		name: 'Github',
+		description: 'Project open sourced on Github',
+		icon: '/icons/github.webp',
+		bgColor: '#fff',
+	},
+	{
+		id: 7,
+		name: 'Open Weather Map',
+		description: 'Real time weather forecast',
+		icon: '/icons/open_weather.webp',
+		bgColor: '#3b38e4',
+	},
+];
+
+export default footerData;
