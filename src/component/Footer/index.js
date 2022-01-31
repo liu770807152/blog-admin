deab4efc3a8b9eaa7e28ae440e2e143c511d4cf5
Bradley <liu770807152@hotmail.com>
2022-01-31 16:36:22 +1100

    make Footer responsive

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -7,56 +7,59 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="flex justify-around">
-        <div className="footer__logo-container">
-          <div className="footer__logo-container--left">
-            <a href="/">
-              <img className="logo" src="/logo_transparent.png" alt="logo"></img>
-            </a>
+      <div className="footer__logo-container">
+        <div className="footer__logo-container--left">
+          <a href="/">
+            <img className="logo" src="/logo_transparent.png" alt="logo"></img>
+          </a>
+        </div>
+        <div className="footer__logo-container--right">
+          <div>
+            <span>Chris Liu</span>
           </div>
-          <div className="footer__logo-container--right">
-            <div>
-              <span>Chris Liu</span>
-            </div>
-            <div>
-              <span>Copyright © 2022</span>
-            </div>
+          <div>
+            <span>Copyright © 2022</span>
           </div>
         </div>
-        <div className="footer__icon-container">
-          <div className="footer__icon-container--inner">
-            <div className="footer__icon-container--list">
-              {footerData.map((item) => (
-                <div
-                  className="footer__icon-container--item"
-                  onMouseEnter={() => setActive(item.id)}
-                  onMouseLeave={() => setActive(null)}
-                  key={item.id}
+      </div>
+      <div className="footer__icon-container">
+        <div className="footer__icon-container--inner">
+          <div className="footer__icon-container--list">
+            {footerData.map((item) => (
+              <div
+                className="footer__icon-container--item"
+                onMouseEnter={() => setActive(item.id)}
+                onMouseLeave={() => setActive(null)}
+                key={item.id}
+                style={{
+                  backgroundColor: `${active === item.id ? item.bgColor : ''}`,
+                }}
+              >
+                <i
+                  className="footer__icon-container--icon"
                   style={{
-                    backgroundColor: `${active === item.id ? item.bgColor : ''}`,
+                    backgroundImage: `url(${item.icon})`,
                   }}
-                >
-                  <i
-                    className="footer__icon-container--icon"
-                    style={{
-                      backgroundImage: `url(${item.icon})`,
-                    }}
-                  />
-                  <div className="footer__icon-container--item-desc">
-                    <div className="footer__icon-container--item-hide-desc"></div>
-                    <div
-                      className="footer__icon-container--item-desc-inner"
-                      style={item.bgColor === '#fff' ? { color: '#333' } : null}
-                    >
-                      <p className="footer__icon-container--name">{item.name}</p>
-                      <p className="footer__icon-container--describe">{item.description}</p>
-                    </div>
+                />
+                <div className="footer__icon-container--item-desc">
+                  <div className="footer__icon-container--item-hide-desc"></div>
+                  <div
+                    className="footer__icon-container--item-desc-inner"
+                    style={item.bgColor === '#fff' ? { color: '#333' } : null}
+                  >
+                    <p className="footer__icon-container--name">{item.name}</p>
+                    <p className="footer__icon-container--describe">{item.description}</p>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
+      </div>
+      <div className="footer__text-container">
+        <p>Actively seeking a full-time position of software engineer</p>
+        <p>Contact me</p>
+        <p>icons</p>
       </div>
     </footer>
   );
